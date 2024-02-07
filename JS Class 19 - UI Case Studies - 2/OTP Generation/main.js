1be51cf8c81248cc2a5452c533516acb5cd321b4
Mohammed <yasermohammeddev@gmail.com>
2024-02-07 16:28:09 +0530

    done

---
+++ JS Class 19 - UI Case Studies - 2/OTP Generation/main.js
@@ -0,0 +1,18 @@
+const inputRef = document.querySelectorAll('.input');
+inputRef.forEach((inputRef) => {
+    inputRef.addEventListener('keyup', function(e) {
+        if (e.target.value && !isNaN(Number(e.target.value)));
+            e.target.nextElementSibling?.focus();
+    })
+})
+
+inputRef.forEach((inputRef) => {
+    inputRef.addEventListener('keydown', function(e) {
+        if (e.key == "Delete" || e.key == "Backspace") {
+            console.log(e.target.value);
+            if (!e.target.value) {
+                e.target.previousElementSibling?.focus();
+            }
+        }
+    })
+})
